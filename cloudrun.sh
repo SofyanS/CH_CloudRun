92b8af385ada16c39fc03d5856a9b8733d6f897e
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-28 14:05:13 -0500

    Cleanup

--- cloudrun.sh
+++ cloudrun.sh
@@ -4,19 +4,8 @@
 	# Create Firestore Native Database
 clientID=$1
 
-# Enable all APIs do this seperately in P1
+# Enable all APIs do this seperately
 gcloud services enable run.googleapis.com
-
-# Build a REST API with Google Container Registry and Cloud Run
-cd pet-theory/lab04
-gcloud builds submit \
-  --tag gcr.io/$DEVSHELL_PROJECT_ID/rest-api
-
-gcloud beta run deploy rest-api \
---image gcr.io/$DEVSHELL_PROJECT_ID/rest-api \
---platform managed \
---region us-central1 \
---allow-unauthenticated
 
 # Create a new GCS bucket with name as <PROJECT_ID>-customer
 gsutil mb gs://$DEVSHELL_PROJECT_ID-customer
@@ -25,8 +14,12 @@ gsutil mb gs://$DEVSHELL_PROJECT_ID-customer
 gsutil cp -r gs://spls/gsp645/2019-10-06T20:10:37_43617/ gs://$DEVSHELL_PROJECT_ID-customer
 gcloud beta firestore import gs://$DEVSHELL_PROJECT_ID-customer/2019-10-06T20:10:37_43617
 
+# Create a new GCS bucket with name as <PROJECT_ID>-public
+gsutil mb gs://$DEVSHELL_PROJECT_ID-public
+gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-public
+
+# Build a REST API with Google Container Registry and Cloud Run
 # Connect REST API to Firestore Database
-# Build and deploy new version of REST API. Have a second index.js in case it's an issue with the code?
 gcloud builds submit \
   --tag gcr.io/$DEVSHELL_PROJECT_ID/rest-api
 
@@ -36,24 +29,21 @@ gcloud beta run deploy rest-api \
 --region us-central1 \
 --allow-unauthenticated
 
-# Add authentication to the REST API
-# Add a web page that lets users sign in and call the REST API
+####################################################
+# Add an authentication check to the REST API code #
+####################################################
+# Modify the index files accordingly
+cd ..
+rm index.js
+mv auth_check.js index.js
 URL=$(gcloud beta run services describe rest-api --platform managed --region us-central1 --format "value(status.url)")
 sed -i "s~REPLACE_URL~$URL~g" website/index.js
 sed -i "s~REPLACE_CLIENTID~$clientID~g" index.js
 
-# Create a new GCS bucket with name as <PROJECT_ID>-public
-gsutil mb gs://$DEVSHELL_PROJECT_ID-public
-gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-public
-
-# Copy website directory content into <PROJECT_ID>-public bucket
+# Copy modified website directory content into <PROJECT_ID>-public bucket
 cd website
 gsutil cp * gs://$DEVSHELL_PROJECT_ID-public
 
-# Add an authentication check to the REST API code
-cd ..
-rm index.js
-mv auth_check.js index.js
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api
 
